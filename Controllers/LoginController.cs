3e39fc178eb3031deb8fa81f11a1dd2fba97a416
LPB\dangnv3 <dangnv3@lpbank.com.vn>
2024-04-03 10:42:22 +0700

    dangnv

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImportFile_excel.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Login( string user, string pass)
+        {
+            try
+            {
+                if(user.Length > 0 && pass.Length > 0)
+                {
+
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return View();  
+        }
+    }
+}
